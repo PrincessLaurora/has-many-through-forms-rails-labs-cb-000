c76ac1e428a8cc0af7cddd322899a55c84133c86
Laura Felce <crepu83@yahoo.it>
2019-06-16 15:17:16 +0000

    Automatically backed up by Learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,10 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   accepts_nested_attributes_for :user, reject_if: :all_blank
 
+  def user_attributes=(attributes)
+    if attributes[:username] != ""
+      user = User.find_or_create_by(username: attributes[:username])
+      self.user = user
+    end
+  end
 end
